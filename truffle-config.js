715fc9459f7a5a836c4e4092d985efd6364923d6
Dylan <38665633+dylie@users.noreply.github.com>
2021-11-17 17:50:18 +0100

    First init

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+// The following code is used as the main truffle config setup to make your dAPP work with etho on metamask.
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+var privateKey = "";
+
+module.exports = {
+    networks: {
+        dev: {
+            provider: () => new HDWalletProvider(privateKey, "https://rpc.ether1.org"),
+			      networkCheckTimeout: 1000000000,
+      timeoutBlocks: 200,
+            network_id: 1313114
+        },
+    },
+	  contracts_directory: './src/contracts',
+      contracts_build_directory: './src/abis',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      version: "^0.8.0" 
+    }
+  }
+};
